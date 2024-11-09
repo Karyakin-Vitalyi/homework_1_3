ae119a057f4daa7ac0fe9e059775c278afa366c5
ВИТАЛИЙ КАРЯКИН <karyakin01@gmail.com>
2024-11-09 18:04:59 +0300

    Практическое задание по работе в Pycharm - "Переменные".

---
+++ main.py
@@ -0,0 +1,7 @@
+#Практическое задание по работе в Pycharm - "Переменные".
+Course = 'Python'
+total_tasks = 12
+hours_spent = 1.5
+average_execution_time = (hours_spent/total_tasks)
+print('Course:',Course, ",", 'Total tasks:', total_tasks ,",", 'hours spent:',
+      hours_spent, "," ,'Average execution time' , average_execution_time ,'our.')
